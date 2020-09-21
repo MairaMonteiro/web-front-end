f6df61659e01cc5ee1d735312f67cccf477c4f08
Maíra <10554201+MairaMonteiro@users.noreply.github.com>
2020-09-20 21:28:19 -0300

    update formulário

--- adalovelace.js
+++ adalovelace.js
@@ -1,5 +1,9 @@
-if(document.getElementById("nome").value != "" && document.getElementById("email").value != ""){
+document.getElementById("botaoEnviar").addEventListener("click", validaForm
 
-}else{
-
+function validaForm(){
+  if(document.getElementById("nome").value != "" && document.getElementById("email").value != ""){
+    alert("Prontinho! Você receberá as novidades por e-mail! ")
+  }else{
+    alert("Por favor, preencha os campos nome e email!")
+  }
 }
