c7bf700f43eacff861c5a6d13f29b4761e562224
Maíra <10554201+MairaMonteiro@users.noreply.github.com>
2020-09-20 21:55:29 -0300

    update telefone

--- adalovelace.js
+++ adalovelace.js
@@ -1,7 +1,8 @@
 document.getElementById("botaoEnviar").addEventListener("click", validaForm
 
 function validaForm(){
-  if(document.getElementById("nome").value != "" && document.getElementById("email").value != ""){
+  if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" 
+  && document.getElementById("phone").value != ""){
     alert("Prontinho! Você receberá as novidades por e-mail! ")
   }else{
     alert("Por favor, preencha os campos nome e email!")
