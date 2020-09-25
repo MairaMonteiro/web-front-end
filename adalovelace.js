1c585597cc3c868849eb85188c23976fce61bb3b
Maíra <10554201+MairaMonteiro@users.noreply.github.com>
2020-09-25 18:21:13 -0300

    bug

--- adalovelace.js
+++ adalovelace.js
@@ -1,6 +1,6 @@
-document.getElementById("botaoEnviar").addEventListener("click", validaForm
+document.getElementById("botaoEnviar").addEventListener("click", validaForm)
 
-function validaForm(){
+function validaForms() {
   if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" 
   && document.getElementById("phone").value != ""){
     alert("Prontinho! Você receberá as novidades por e-mail! ")
