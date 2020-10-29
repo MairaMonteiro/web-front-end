1cec7dde687de2da4f1b21338ed024666bcf1a10
Maíra Monteiro <mairamonte@gmail.com>
2020-10-28 21:18:59 -0300

    treino de javascript

---
+++ treino.js
@@ -0,0 +1,13 @@
+console.log("Hello World!"); //hjg
+
+
+var one = 1;
+let tzo = "2";
+let three = 3;
+var ourDecimal = 5.7;
+var remainder = 11 % 3;
+var myStr = "I am 'minx' right?"
+var firstNLength = myStr.length;
+
+console.log((one + three) * tzo);
+console.log(firstNLength);
