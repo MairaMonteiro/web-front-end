bf0e3d069cacbdad5032c40969624aa3eaa5af87
Maíra Monteiro <mairamonte@gmail.com>
2020-11-29 19:39:03 -0300

    Create pti.js

---
+++ pti.js
@@ -0,0 +1,17 @@
+alert("Produção Textual Individual /n ALGORITMOS E PROGRAMAÇÃO I");
+
+console.log("Qual vale mais a pena? /nGasolina ou Alcool?");
+console.log("Qual o preço da gasolina?");
+var precoGas;
+console.log("Qual o preço da gasolina?");
+var precoAlcool;
+
+var comparativo = precoAlcool/precoGas
+
+if (comparativo > 0.7){
+    console.log("Vale a pena abastecer com gasolina ")
+} else if (comparativo = 0.7){
+    console.log("Pode abastecer Álcool ou Gasolina, de acordo com sua preferência")
+} else {
+    console.log("Vale a pena abastecer com álcool")
+}
